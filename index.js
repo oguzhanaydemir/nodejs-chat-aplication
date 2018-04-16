c75fc26e55755ffbb87ae553ef116ff2e026ebd4
Oğuzhan Aydemir <ouzhanaydemir7@gmail.com>
2018-04-16 16:39:39 +0300

    Fix database errors

--- index.js
+++ index.js
@@ -22,26 +22,39 @@ app.get('/', function (req, res) {
 
 io.sockets.on('connection', function (socket) {
     console.log('Someone is connected.');
-/*     .push(socket);
-    console.log('Total connections: ' + connections.length); */
+    /*     .push(socket);
+        console.log('Total connections: ' + connections.length); */
 
     socket.on('disconnect', function (data) {
-        
+
     });
 
     socket.on('send message', function (data, callback) {
         console.log("Request Received Message: " + JSON.stringify(data, "", 2));
         users.createMessage(data, function (err) {
             if (err) {
-                console.log (err);
-
+                console.log(err);
                 data.from = "Server";
                 data.body = "ERROR";
             }
             else {
                 console.log("Message has been written to database.");
-                socket.broadcast.emit('new message', data);
-                console.log("Broadcasting Received Message: " + JSON.stringify(data, "", 2));
+                if (data.type === 0) { //Global messge
+                    socket.broadcast.emit('new message', data);
+                    console.log("Broadcasting Received Message: " + JSON.stringify(data, "", 2));
+                } else if (data.type === 2) { //Private message
+                    users.getUser(data.to, function(err, response){
+                        if (err) {
+                            console.log(err);
+                        }
+                        else{
+                            console.log("\n\nReceived User Data: " + response.socket_id +"\n\n");
+                        }
+                    })
+                    console.log("User sent message from " + data.from + " to " + data.to);
+                    socket.broadcast.to().emit('message', 'for your eyes only');
+
+                }
             }
             callback(data);
         });
@@ -49,15 +62,15 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on('new user', function (username, callback) {
-        console.log('Request Received Username: ' + username);
-        var control = users.getUser(username, function (err, userdata) {
+        users.getUser(username, function (err, userdata) {
             if (err) {
                 console.log(err);
             }
-            else if (userdata.length === 0) {
+            else if (userdata === null) {
                 //Create New User
                 console.log("User Not Found Create New User");
                 var dbData = {
+                    socket_id: socket.id,
                     name: username,
                     groups: [],
                     messages: []
--- models/users.js
+++ models/users.js
@@ -4,18 +4,19 @@ const mongoose = require('mongoose');
 const messageSchema = mongoose.Schema({
     body: String,
     type: Number,
-    from: String,
-    to: String
+    to: String,
+    from: String
 });
 
 //Group Schema
 const groupSchema = mongoose.Schema({
-    name: String
+    groupName: String
 });
 
 //User Schmea
 const userSchema = mongoose.Schema({
     name: String,
+    socket_id:String,
     groups: [groupSchema],
     messages: [messageSchema]
 });
@@ -28,9 +29,29 @@ module.exports.getUser = (username, callback) => {
     User.findOne({ name: username }, callback);
 }
 
+
 //Create User
 module.exports.createUser = (data, callback) => {
     User.create(data, callback);
+}
+
+// Get Groups
+module.exports.getGroup = (name, callback) => {
+    User.findOne({ 'groups.groupName': name }, callback);
+}
+
+
+//Create Group
+module.exports.createGroup = (username, name, callback) => {
+    User.update({ name: username },
+        {
+            $push: {
+                groups: {
+                    groupName: name
+                }
+            }
+        },
+        callback);
 }
 
 //Create Message
@@ -44,6 +65,67 @@ module.exports.createMessage = (data, callback) => {
             },
             { multi: true },
             callback);
+    }
+    else if (data.type === 1) {//Group Emit
+        var groupName = data.to;
+
+        User.getGroup(groupName, function (err, group) {
+            if (group === null) {//Create Group
+                console.log("Group not found: " + groupName);
+                console.log("Creating new group: " + groupName + " ...");
+                User.createGroup(data.from, groupName, function (err) {
+                    if (err) {
+                        console.log("Group couldn't created: " + groupName);
+                    } else {
+                        console.log("Group " + groupName + " is created by " + data.from);
+                    }
+                });
+            }
+
+            //Get Existing Group and send group message
+            User.update({ 'groups.groupName': data.to },
+                {
+                    $push: {
+                        messages: data
+                    }
+                },
+                { multi: true },
+                callback);
+
+        });
+    } else if (data.type === 2) {//Send to private message
+        var to = data.to;
+        var from = data.from;
+        var body = data.body;
+        console.log("This message to " + to);
+        console.log("Trying to sending message from " + from + " to " + to);
+        User.getUser(to, function(err, user) {
+            if (err) {
+                console.log("User not found: " + to);
+            } else {
+                User.update({name:data.to},
+                    
+                    
+                {
+                    $push: {
+                        messages: data
+                    }
+                },
+              
+                callback);
+
+                User.update({name:data.from},
+                    
+                    
+                    {
+                        $push: {
+                            messages: data
+                        }
+                    },
+                  
+                    callback);
+            }
+        });
     }
 
 }
